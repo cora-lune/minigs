b3d39f724f9d8aa29aff55fc612028f8efc27cdd
Coraline <coraline@guides-shopping.com>
2017-10-25 16:50:15 +0200

    add ProductsValidation

--- src/AppBundle/Controller/ProductsController.php
+++ src/AppBundle/Controller/ProductsController.php
@@ -11,6 +11,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Products;
 use AppBundle\Form\ProductsType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -19,14 +20,30 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class ProductsController extends Controller
 {
     /**
-     * @Route("/products")
+     * @Route("/products", name="addProducts")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
 
-    public function addAction()
+    public function addAction(Request $request)
     {
         $products = new Products();
 
         $form = $this->createForm(ProductsType::class, $products);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())  {
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($products);
+            $em->flush();
+
+            return $this->render('default/productsValidation.html.twig');
+        }
 
 
         return $this->render('default/productsAdd.html.twig', array('form' => $form->createView()));
